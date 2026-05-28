c1e80216c6e8c7a959a3f8c9011ef334dbe9833d
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-05-28 09:11:53 +0300

    fix: update router

--- src/router.rs
+++ src/router.rs
@@ -6,30 +6,24 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     let v1 = web::scope("/v1");
 
     // Experiences
-    let v1 = v1
-        .service(
-            web::scope("/experiences")
-                .service(experiences::many)
-                .service(experiences::one),
-        )
-        .service(
-            web::scope("/experiences")
-                .guard(guard::Header("content-type", "json/application"))
-                .service(experiences::insert)
-                .service(experiences::update)
-                .service(experiences::delete),
-        );
+    let v1 = v1.service(
+        web::scope("/experiences")
+            .service(experiences::many)
+            .service(experiences::one)
+            .service(experiences::insert)
+            .service(experiences::update)
+            .service(experiences::delete),
+    );
 
     // Tags
-    let v1 = v1
-        .service(web::scope("/tags").service(tags::many).service(tags::one))
-        .service(
-            web::scope("/tags")
-                .guard(guard::Header("content-type", "json/application"))
-                .service(tags::insert)
-                .service(tags::update)
-                .service(tags::delete),
-        );
+    let v1 = v1.service(
+        web::scope("/tags")
+            .service(tags::many)
+            .service(tags::one)
+            .service(tags::insert)
+            .service(tags::update)
+            .service(tags::delete),
+    );
 
     cfg.service(web::scope("/api").service(v1));
 }
