aad92523a857291ef2da81652dadb99bb93de10f
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-04 22:17:32 +0300

    refactor: update experiences-tags repository to comment out redundant methods

--- src/modules/experiences_tags/repository.rs
+++ src/modules/experiences_tags/repository.rs
@@ -1,41 +1,41 @@
 use diesel::{
-    BoolExpressionMethods, Connection, ExpressionMethods, OptionalExtension, QueryDsl, RunQueryDsl,
-    SelectableHelper, result::Error, upsert::excluded,
+    BoolExpressionMethods, Connection, ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper,
+    result::Error, upsert::excluded,
 };
 
 use super::ExperienceTag;
 use crate::{PooledConn, schema::experiences_tags};
 
-pub fn many_by_experience_id(
-    conn: &mut PooledConn,
-    experience_id: i32,
-) -> Result<Vec<ExperienceTag>, Error> {
-    experiences_tags::table
-        .filter(experiences_tags::experience_id.eq(experience_id))
-        .get_results(conn)
-}
+// pub fn many_by_experience_id(
+//     conn: &mut PooledConn,
+//     experience_id: i32,
+// ) -> Result<Vec<ExperienceTag>, Error> {
+//     experiences_tags::table
+//         .filter(experiences_tags::experience_id.eq(experience_id))
+//         .get_results(conn)
+// }
 
-pub fn many_by_tag_id(conn: &mut PooledConn, tag_id: i32) -> Result<Vec<ExperienceTag>, Error> {
-    experiences_tags::table
-        .filter(experiences_tags::tag_id.eq(tag_id))
-        .get_results(conn)
-}
+// pub fn many_by_tag_id(conn: &mut PooledConn, tag_id: i32) -> Result<Vec<ExperienceTag>, Error> {
+//     experiences_tags::table
+//         .filter(experiences_tags::tag_id.eq(tag_id))
+//         .get_results(conn)
+// }
 
-pub fn one(
-    conn: &mut PooledConn,
-    tag_id: i32,
-    experience_id: i32,
-) -> Result<Option<ExperienceTag>, Error> {
-    experiences_tags::table
-        .filter(experiences_tags::tag_id.eq(tag_id))
-        .filter(experiences_tags::experience_id.eq(experience_id))
-        .get_result(conn)
-        .optional()
-}
+// pub fn one(
+//     conn: &mut PooledConn,
+//     tag_id: i32,
+//     experience_id: i32,
+// ) -> Result<Option<ExperienceTag>, Error> {
+//     experiences_tags::table
+//         .filter(experiences_tags::tag_id.eq(tag_id))
+//         .filter(experiences_tags::experience_id.eq(experience_id))
+//         .get_result(conn)
+//         .optional()
+// }
 
-pub fn many(conn: &mut PooledConn) -> Result<Vec<ExperienceTag>, Error> {
-    experiences_tags::table.get_results(conn)
-}
+// pub fn many(conn: &mut PooledConn) -> Result<Vec<ExperienceTag>, Error> {
+//     experiences_tags::table.get_results(conn)
+// }
 
 pub fn insert_one(
     conn: &mut PooledConn,
@@ -82,25 +82,25 @@ pub fn replace_many(
     })
 }
 
-pub fn delete_by_experience_id(
-    conn: &mut PooledConn,
-    experience_id: i32,
-) -> Result<Vec<ExperienceTag>, Error> {
-    diesel::delete(
-        experiences_tags::dsl::experiences_tags
-            .filter(experiences_tags::experience_id.eq(experience_id)),
-    )
-    .returning(ExperienceTag::as_returning())
-    .get_results(conn)
-}
+// pub fn delete_by_experience_id(
+//     conn: &mut PooledConn,
+//     experience_id: i32,
+// ) -> Result<Vec<ExperienceTag>, Error> {
+//     diesel::delete(
+//         experiences_tags::dsl::experiences_tags
+//             .filter(experiences_tags::experience_id.eq(experience_id)),
+//     )
+//     .returning(ExperienceTag::as_returning())
+//     .get_results(conn)
+// }
 
-pub fn delete_by_tag_id(conn: &mut PooledConn, tag_id: i32) -> Result<Vec<ExperienceTag>, Error> {
-    diesel::delete(
-        experiences_tags::dsl::experiences_tags.filter(experiences_tags::tag_id.eq(tag_id)),
-    )
-    .returning(ExperienceTag::as_returning())
-    .get_results(conn)
-}
+// pub fn delete_by_tag_id(conn: &mut PooledConn, tag_id: i32) -> Result<Vec<ExperienceTag>, Error> {
+//     diesel::delete(
+//         experiences_tags::dsl::experiences_tags.filter(experiences_tags::tag_id.eq(tag_id)),
+//     )
+//     .returning(ExperienceTag::as_returning())
+//     .get_results(conn)
+// }
 
 pub fn delete(
     conn: &mut PooledConn,
