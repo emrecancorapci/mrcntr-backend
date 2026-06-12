853624fd90d2c61a09792584f8fee30753687771
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-12 23:24:03 +0300

    fix: fix the router (again)

--- src/router.rs
+++ src/router.rs
@@ -9,54 +9,50 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/api").service(
             web::scope("/v1")
-                // Experiences - Public
+                // PUBLIC
+                .service(
+                    web::scope("/auth")
+                        .service(auth::login)
+                        .service(auth::register),
+                )
+                // PARTLY PROTECTED
                 .service(
                     web::scope("/experiences")
                         .service(experiences::many)
-                        .service(experiences::one),
+                        .service(experiences::one)
+                        .service(
+                            web::scope("")
+                                .wrap(from_fn(auth_middleware))
+                                .service(experiences::insert)
+                                .service(experiences::update)
+                                .service(experiences::delete),
+                        ),
                 )
-                // Experiences - Protected
-                .service(
-                    web::scope("/experiences")
-                        .wrap(from_fn(auth_middleware))
-                        .service(experiences::insert)
-                        .service(experiences::update)
-                        .service(experiences::delete),
-                )
-                // Tags - Public
-                .service(web::scope("/tags").service(tags::many).service(tags::one))
-                // Tags - Protected
                 .service(
                     web::scope("/tags")
-                        .wrap(from_fn(auth_middleware))
-                        .service(tags::insert)
-                        .service(tags::update)
-                        .service(tags::delete),
+                        .service(tags::many)
+                        .service(tags::one)
+                        .service(
+                            web::scope("")
+                                .wrap(from_fn(auth_middleware))
+                                .service(tags::insert)
+                                .service(tags::update)
+                                .service(tags::delete),
+                        ),
                 )
-                // Experiences-Tags - Protected
-                .service(
-                    web::scope("/experiences-tags")
-                        .wrap(from_fn(auth_middleware))
-                        .service(experiences_tags::insert_one)
-                        .service(experiences_tags::insert_many)
-                        .service(experiences_tags::replace_many_by_experience_id)
-                        .service(experiences_tags::delete),
-                )
-                // Categories - Public
                 .service(
                     web::scope("/categories")
                         .service(categories::many)
-                        .service(categories::one),
+                        .service(categories::one)
+                        .service(
+                            web::scope("")
+                                .wrap(from_fn(auth_middleware))
+                                .service(categories::insert)
+                                .service(categories::update)
+                                .service(categories::delete),
+                        ),
                 )
-                // Categories - Protected
-                .service(
-                    web::scope("/categories")
-                        .wrap(from_fn(auth_middleware))
-                        .service(categories::insert)
-                        .service(categories::update)
-                        .service(categories::delete),
-                )
-                // User - Protected
+                // FULLY PROTECTED
                 .service(
                     web::scope("/user")
                         .wrap(from_fn(auth_middleware))
@@ -66,11 +62,13 @@ pub fn routes(cfg: &mut web::ServiceConfig) {
                         .service(users::update)
                         .service(users::delete),
                 )
-                // Auth - Public
                 .service(
-                    web::scope("/auth")
-                        .service(auth::login)
-                        .service(auth::register),
+                    web::scope("/experiences-tags")
+                        .wrap(from_fn(auth_middleware))
+                        .service(experiences_tags::insert_one)
+                        .service(experiences_tags::insert_many)
+                        .service(experiences_tags::replace_many_by_experience_id)
+                        .service(experiences_tags::delete),
                 ),
         ),
     );
