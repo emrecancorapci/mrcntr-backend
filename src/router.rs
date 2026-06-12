f2b4b9081e8047e12cdc30513a176ce54bc8add7
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-12 22:17:58 +0300

    feat: update router to add protected routes

--- src/router.rs
+++ src/router.rs
@@ -1,61 +1,77 @@
-use actix_web::web;
+use actix_web::{middleware::from_fn, web};
 
-use crate::modules::{auth, categories, experiences, experiences_tags, tags, users};
+use crate::{
+    middlewares::auth::auth_middleware,
+    modules::{auth, categories, experiences, experiences_tags, tags, users},
+};
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
-    let v1 = web::scope("/v1");
-
-    // Experiences
-    let v1 = v1.service(
-        web::scope("/experiences")
-            .service(experiences::many)
-            .service(experiences::one)
-            .service(experiences::insert)
-            .service(experiences::update)
-            .service(experiences::delete),
+    cfg.service(
+        web::scope("/api").service(
+            web::scope("/v1")
+                // Experiences - Public
+                .service(
+                    web::scope("/experiences")
+                        .service(experiences::many)
+                        .service(experiences::one),
+                )
+                // Experiences - Protected
+                .service(
+                    web::scope("/experiences")
+                        .wrap(from_fn(auth_middleware))
+                        .service(experiences::insert)
+                        .service(experiences::update)
+                        .service(experiences::delete),
+                )
+                // Tags - Public
+                .service(web::scope("/tags").service(tags::many).service(tags::one))
+                // Tags - Protected
+                .service(
+                    web::scope("/tags")
+                        .wrap(from_fn(auth_middleware))
+                        .service(tags::insert)
+                        .service(tags::update)
+                        .service(tags::delete),
+                )
+                // Experiences-Tags - Protected
+                .service(
+                    web::scope("/experiences-tags")
+                        .wrap(from_fn(auth_middleware))
+                        .service(experiences_tags::insert_one)
+                        .service(experiences_tags::insert_many)
+                        .service(experiences_tags::replace_many_by_experience_id)
+                        .service(experiences_tags::delete),
+                )
+                // Categories - Public
+                .service(
+                    web::scope("/categories")
+                        .service(categories::many)
+                        .service(categories::one),
+                )
+                // Categories - Protected
+                .service(
+                    web::scope("/categories")
+                        .wrap(from_fn(auth_middleware))
+                        .service(categories::insert)
+                        .service(categories::update)
+                        .service(categories::delete),
+                )
+                // User - Protected
+                .service(
+                    web::scope("/user")
+                        .wrap(from_fn(auth_middleware))
+                        .service(users::many)
+                        .service(users::one)
+                        .service(users::insert)
+                        .service(users::update)
+                        .service(users::delete),
+                )
+                // Auth - Public
+                .service(
+                    web::scope("/auth")
+                        .service(auth::login)
+                        .service(auth::register),
+                ),
+        ),
     );
-
-    // Tags
-    let v1 = v1.service(
-        web::scope("/tags")
-            .service(tags::many)
-            .service(tags::one)
-            .service(tags::insert)
-            .service(tags::update)
-            .service(tags::delete),
-    );
-
-    let v1 = v1.service(
-        web::scope("/experiences-tags")
-            .service(experiences_tags::insert_one)
-            .service(experiences_tags::insert_many)
-            .service(experiences_tags::replace_many_by_experience_id)
-            .service(experiences_tags::delete),
-    );
-
-    let v1 = v1.service(
-        web::scope("/categories")
-            .service(categories::many)
-            .service(categories::one)
-            .service(categories::insert)
-            .service(categories::update)
-            .service(categories::delete),
-    );
-
-    let v1 = v1.service(
-        web::scope("/user")
-            .service(users::many)
-            .service(users::one)
-            .service(users::insert)
-            .service(users::update)
-            .service(users::delete),
-    );
-
-    let v1 = v1.service(
-        web::scope("/auth")
-            .service(auth::register)
-            .service(auth::login),
-    );
-
-    cfg.service(web::scope("/api").service(v1));
 }
