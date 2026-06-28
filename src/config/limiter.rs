452accdd15f7529fc3da8ebad6abd2c3f212c7b3
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-06-29 00:26:48 +0300

    refactor: update limiter.rs to turn limiter function into struct

--- src/config/limiter.rs
+++ src/config/limiter.rs
@@ -4,17 +4,21 @@ use actix_limitation::Limiter;
 use actix_session::SessionExt;
 use actix_web::dev::ServiceRequest;
 
-pub fn build_limiter() -> Limiter {
-    let redis_url = std::env::var("REDIS_URL").expect("DATABASE_URL must be set");
+pub struct AppLimiter;
 
-    Limiter::builder(redis_url)
-        .key_by(|req: &ServiceRequest| {
-            req.get_session()
-                .get("session-id")
-                .unwrap_or_else(|_| req.cookie("rate-api-id").map(|c| c.to_string()))
-        })
-        .limit(5000)
-        .period(Duration::from_secs(3600)) // 60 minutes
-        .build()
-        .unwrap()
+impl AppLimiter {
+    pub fn build() -> Limiter {
+        let redis_url = std::env::var("REDIS_URL").expect("DATABASE_URL must be set");
+
+        Limiter::builder(redis_url)
+            .key_by(|req: &ServiceRequest| {
+                req.get_session()
+                    .get("session-id")
+                    .unwrap_or_else(|_| req.cookie("rate-api-id").map(|c| c.to_string()))
+            })
+            .limit(5000)
+            .period(Duration::from_secs(3600)) // 60 minutes
+            .build()
+            .unwrap()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_limitation::RateLimiter;
 use actix_web::{App, HttpServer, middleware::Logger, web};
-use mrcntr::AppDatabase;
+use mrcntr::{AppDatabase, AppLimiter};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -12,12 +12,13 @@ async fn main() -> std::io::Result<()> {
     let redis_pool = AppDatabase::redis_conn();
     let shared_redis_pool = web::Data::new(redis_pool);
 
-    let limiter = web::Data::new(mrcntr::build_limiter());
+    let limiter = AppLimiter::build();
+    let shared_limiter = web::Data::new(limiter);
 
     let app = move || {
         App::new()
             .wrap(RateLimiter::default())
-            .app_data(limiter.clone())
+            .app_data(shared_limiter.clone())
             .wrap(Logger::default())
             .app_data(shared_db_pool.clone())
             .app_data(shared_redis_pool.clone())
