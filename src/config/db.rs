75ff7721a023022df6f5713ee4b434339f4fa201
Emre Can Çorapçı <emrecancorapci@gmail.com>
2026-07-01 12:32:21 +0300

    refactor: move db functions into db.rs, replace pool types

--- src/config/db.rs
+++ src/config/db.rs
@@ -1,16 +1,24 @@
 use deadpool_redis::{Config, Runtime};
-use diesel::{
-    PgConnection,
-    r2d2::{ConnectionManager, PooledConnection},
-};
+use diesel_async::AsyncPgConnection;
+use diesel_async::pooled_connection::AsyncDieselConnectionManager;
+use diesel_async::pooled_connection::deadpool::Pool;
 
-pub type DbPool = diesel::r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbPool = Pool<AsyncPgConnection>;
+pub type PooledConn = AsyncPgConnection;
 pub type RedisPool = deadpool_redis::Pool;
-pub type PooledConn = PooledConnection<ConnectionManager<PgConnection>>;
 
 pub struct AppDatabase;
 
 impl AppDatabase {
+    pub fn db_conn() -> DbPool {
+        let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let config = AsyncDieselConnectionManager::new(database_url);
+
+        Pool::builder(config)
+            .build()
+            .expect("Failed to create Database connection pool")
+    }
+
     pub fn redis_conn() -> RedisPool {
         let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
         let config = Config::from_url(redis_url);
